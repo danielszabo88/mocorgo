10e9bc495e7928987b9f04abdacbf45e4c58d360
danielszabo88 <63491859+danielszabo88@users.noreply.github.com>
2020-06-28 16:26:34 +0630

    fewer lines
    same functionality but in 20 lines only (looks simpler)

--- Game Template/game_template.js
+++ Game Template/game_template.js
@@ -1,22 +1,18 @@
 //STEP 1: setting up the environment
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-//
+
 // creating the starting objects and variables here before starting the main loop
 // for example: 
 putWallsAround(0, 0, canvas.clientWidth, canvas.clientHeight);
 let player = new Ball(100, 100, 30, 5);
-player.player = true;
 player.color = "red";
 player.maxSpeed = 5;
 
-
 //STEP 2: defining the game logic
 function gameLogic(){
-    //
     // this gets called preiodically as part of the main loop
     // define the rules here
-    //
 }
 
 //STEP 3: handling the user input and the game loop
