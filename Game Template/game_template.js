40529b7d7e543f913313b4f51dd8c81d340d5094
danielszabo88 <63491859+danielszabo88@users.noreply.github.com>
2021-06-29 20:28:00 +0200

    changing keyCode to code
    keyCode has been deprecated

--- Game Template/game_template.js
+++ Game Template/game_template.js
@@ -1,20 +1,21 @@
-//STEP 1: setting up the environment
+// STEP 1: setting up the environment
+// creating the starting objects and variables before starting the main loop
+// for example: 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+canvas.focus();
 
-// creating the starting objects and variables here before starting the main loop
-// for example: 
 putWallsAround(0, 0, canvas.clientWidth, canvas.clientHeight);
 let player = new Ball(100, 100, 30, 5);
 player.color = "red";
 player.maxSpeed = 5;
 
-//STEP 2: defining the game logic
+// STEP 2: defining the game logic
 function gameLogic(){
     // this gets called preiodically as part of the main loop
     // define the rules here
 }
 
-//STEP 3: handling the user input and the game loop
+// STEP 3: handling the user input and the game loop
 userInput(player);
 requestAnimationFrame(mainLoop);
--- Game Template/userInput.js
+++ Game Template/userInput.js
@@ -1,37 +1,37 @@
 //Event listeners for the arrow keys
 function userInput(obj){
     canvas.addEventListener('keydown', function(e){
-        if(e.keyCode === 37){
+        if(e.code === "ArrowLeft"){
             obj.left = true;
         }
-        if(e.keyCode === 38){
+        if(e.code === "ArrowUp"){
             obj.up = true;
         }
-        if(e.keyCode === 39){
+        if(e.code === "ArrowRight"){
             obj.right = true;
         }
-        if(e.keyCode === 40){
+        if(e.code === "ArrowDown"){
             obj.down = true;
         }
-        if(e.keyCode === 32){
+        if(e.code === "Space"){
             obj.action = true;
         }
     });
     
     canvas.addEventListener('keyup', function(e){
-        if(e.keyCode === 37){
+        if(e.code === "ArrowLeft"){
             obj.left = false;
         }
-        if(e.keyCode === 38){
+        if(e.code === "ArrowUp"){
             obj.up = false;
         }
-        if(e.keyCode === 39){
+        if(e.code === "ArrowRight"){
             obj.right = false;
         }
-        if(e.keyCode === 40){
+        if(e.code === "ArrowDown"){
             obj.down = false;
         }
-        if(e.keyCode === 32){
+        if(e.code === "Space"){
             obj.action = false;
         }
     });    
